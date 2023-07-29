341d522bf2a8df0476d838faf8226c6dff1acac5
Podexus23 <santon23@gmail.com>
2023-07-29 21:01:29 +0300

    feat: add functionality

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModalWindow = e => {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+};
+
+const closeModalWindow = e => {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(button => {
+  button.addEventListener('click', openModalWindow);
+});
+
+btnCloseModal.addEventListener('click', closeModalWindow);
+overlayEl.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape' && !modalEl.classList.contains('hidden'))
+    closeModalWindow(e);
+});
